46288f2440dd02ee2174863c4fc5e293f79d66bb
TJTJ-cpu <117895770+TJTJ-cpu@users.noreply.github.com>
2026-02-10 15:27:19 +0100

    Feature: Added add a random book func

--- BookStore.Api/Data/DataExtensions.cs
+++ BookStore.Api/Data/DataExtensions.cs
@@ -28,17 +28,38 @@ public static class DataExtensions
                 if (!context.Set<Author>().Any())
                 {
                     context.Set<Author>().AddRange(
-                        new Author { Name = "TJ Mady" },
-                        new Author { Name = "Stephen R. Covey" },
-                        new Author { Name = "Viktor E. Frankl" },
-                        new Author { Name = "Marcus Aurelius" },
-                        new Author { Name = "Jordan Peterson" },
-                        new Author { Name = "Tim Ferriss" },
-                        new Author { Name = "David Goggins" },
-                        new Author { Name = "Mel Robbins" },
-                        new Author { Name = "Robert Cialdini" },
-                        new Author { Name = "Ryan Holiday" },
-                        new Author { Name = "Robin Sharma" }
+                            new Author { Name = "Stephen R. Covey" },      // ID 1
+                            new Author { Name = "Viktor E. Frankl" },      // ID 2
+                            new Author { Name = "Marcus Aurelius" },       // ID 3
+                            new Author { Name = "Jordan Peterson" },       // ID 4
+                            new Author { Name = "Tim Ferriss" },           // ID 5
+                            new Author { Name = "David Goggins" },         // ID 6
+                            new Author { Name = "Mel Robbins" },           // ID 7
+                            new Author { Name = "Robert Cialdini" },       // ID 8
+                            new Author { Name = "Ryan Holiday" },          // ID 9
+                            new Author { Name = "Robin Sharma" },          // ID 10
+
+                            new Author { Name = "Tara Westover" },         // ID 11
+                            new Author { Name = "Thibaut Meurisse" },      // ID 12
+                            new Author { Name = "Ichiro Kishimi" },        // ID 13
+                            new Author { Name = "Brianna Wiest" },         // ID 14
+                            new Author { Name = "Matthew Hussey" },        // ID 15
+                            new Author { Name = "Fumio Sasaki" },          // ID 16
+                            new Author { Name = "M. Scott Peck" },         // ID 17
+                            new Author { Name = "Noah Kagan" },            // ID 18
+                            new Author { Name = "Anthony Youn" },          // ID 19
+                            new Author { Name = "Oliver Burkeman" },       // ID 20
+                            new Author { Name = "Jia Jiang" },             // ID 21
+                            new Author { Name = "Richard Koch" },          // ID 22
+                            new Author { Name = "R.B. Sparkman" },         // ID 23
+                            new Author { Name = "Eva Illouz" },            // ID 24
+                            new Author { Name = "David Deida" },           // ID 25
+                            new Author { Name = "Daniel J. Siegel" },      // ID 26
+                            new Author { Name = "Henry Cloud" },           // ID 27
+                            new Author { Name = "Adam Grant" },            // ID 28
+                            new Author { Name = "Thomas Erikson" },        // ID 29
+                            new Author { Name = "Paul Millerd" },          // ID 30
+                            new Author { Name = "Anna Lembke" }            // ID 31
 
                     );
                     context.SaveChanges();
